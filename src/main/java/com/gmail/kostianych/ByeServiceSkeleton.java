8b68b860464d4edf2f3a8ac01013001f59e05b57
kostianych <kostianych@gmail.com>
2012-03-29 17:04:58 +0300

    Package name is fixed

--- src/main/java/com/gmail/kostianych/ByeServiceSkeleton.java
+++ src/main/java/com/gmail/kostianych/ByeServiceSkeleton.java
@@ -5,7 +5,7 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.5.1  Built on : Oct 19, 2009 (10:59:00 EDT)
  */
-    package com.gmail.kostianych.ws;
+    package com.gmail.kostianych;
     /**
      *  ByeServiceSkeleton java skeleton for the axisService
      */
